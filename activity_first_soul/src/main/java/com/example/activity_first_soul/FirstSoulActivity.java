3d2251d5470fcef3bd00ae4db8527d17077ef97c
l0git <l0git@gmail.com>
2022-05-13 12:22:45 +0300

    first commit

--- activity_first_soul/src/main/java/com/example/activity_first_soul/FirstSoulActivity.java
+++ activity_first_soul/src/main/java/com/example/activity_first_soul/FirstSoulActivity.java
@@ -1,14 +1,14 @@
-package com.example.cheat_sheet_on_dark_souls;
+package com.example.activity_first_soul;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FirstSoulActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first_soul);
     }
 }
