3166b3d1ef96c5f40234a0aef4d72570feca7d9f
F1lterEr <98636299+F1lterEr@users.noreply.github.com>
2022-05-15 23:36:34 +0300

    Добавлены активити 4 и 5

---
+++ activity_fourth_soul/src/main/java/com/example/activity_fourth_soul/FourthSoulActivity.java
@@ -0,0 +1,14 @@
+package com.example.activity_fourth_soul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FourthSoulActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth_soul);
+    }
+}
